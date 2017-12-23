ef18c449f7dc24773f3cf555097cbcf11f644b38
TOKdawn <dawninshadow0.0@gmail.com>
2017-12-23 23:22:28 +0800

    更新底部信息

--- Photo_frame/src/components/before.vue
+++ Photo_frame/src/components/before.vue
@@ -1,15 +1,16 @@
 <template>
   <el-container>
   <el-header  id="header"> 
-    <el-carousel indicator-position="outside" arrow="never" height="60vh" >
+    <el-carousel indicator-position="outside" arrow="never" height="65vh" >
     <el-carousel-item v-for="item in 4" :key="item">
-      <h3>{{ item }}</h3>
+      <h3></h3>
     </el-carousel-item>
   </el-carousel>
   </el-header>
   <el-main><p>生成你的大东软个性贺卡</p>
   <el-button type="warning" round @click="link">制作海报</el-button>
   </el-main>
+  <div id="footer">策划: 品牌发展部&nbsp 研发:微光网络工作室</div>
 </el-container>
 </template>
 
@@ -24,9 +25,9 @@ export default {
 </script>
 <style>
 #header{
-  height: 70vh !important;
+  height: 75vh !important;
   background-color: chocolate;
-  padding-top: 5vh;
+  padding-top: 3vh;
 }
 .el-carousel__item h3 {
     color: #475669;
@@ -35,7 +36,9 @@ export default {
     line-height: 200px;
     margin: 0;
   }
-  
+  .el-main{
+    padding: 10px;
+  }
   .el-carousel__item:nth-child(1) {
     background:url(./1.jpg) 0 0/100% 100% no-repeat;
   }
@@ -51,5 +54,15 @@ export default {
   }
   .el-header{
     padding: 0 35px;
+  }
+  #footer{
+    position: absolute;
+    display: block;
+    font-size: 10px;
+    text-align: center;
+    bottom: 0px;
+    width: 100%;
+    background-color: #e1e1e1;
+    color: #888;
   }
 </style>
